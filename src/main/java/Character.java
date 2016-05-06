bddf699e477106f8fd200721289dac86d7cbc8ff
Tera <Tera>
2016-05-06 18:17:24 +0800

    add name

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -20,8 +20,8 @@ public class Character implements Comparable<Character> {
 		this.name = name;
 		this.colour = colour;
 		this.id = id;
-		this.x = 50+(id/10)*60;
-		this.y = 50+(id%10)*60;
+		this.x = 50 + (id / 10) * 60;
+		this.y = 50 + (id % 10) * 60;
 		targets = new ArrayList<Character>();
 
 	}
@@ -29,8 +29,8 @@ public class Character implements Comparable<Character> {
 	public void setLinkWeight(int value) {
 		this.value = value;
 	}
-	
-	public void setPosition(float x, float y){
+
+	public void setPosition(float x, float y) {
 		this.x = x;
 		this.y = y;
 	}
@@ -40,18 +40,18 @@ public class Character implements Comparable<Character> {
 		int hi = PApplet.unhex(colour.substring(1, 9));
 		this.parent.fill(hi, 200);
 		this.parent.ellipse(x, y, 50, 50);
-		/*
+
 		this.parent.fill(153, 153, 255);
-		this.parent.ellipse(x+30, y-15,name.length()*10, 25);
-		
+		this.parent.rect(x + 10, y - 25, name.length() * 10, 25, 12, 12, 12, 12);
+
 		this.parent.fill(173, 254, 220);
-		if(id==0)this.parent.text(name, x, y-10);
 		this.parent.textSize(12);
-		
+		this.parent.text(name, x + 18, y - 8);
+
 		for (Character character : targets) {
 			this.parent.line(this.x, this.y, character.x, character.y);
 			this.parent.strokeWeight(this.value);
-		}*/
+		}
 
 	}
 
@@ -62,10 +62,13 @@ public class Character implements Comparable<Character> {
 	public ArrayList<Character> getTargets() {
 		return this.targets;
 	}
-	
-	public int compareTo(Character c){
-		if(this.id>c.id) return 1;
-		else if(this.id==c.id) return 0;
-		else return -1;
+
+	public int compareTo(Character c) {
+		if (this.id > c.id)
+			return 1;
+		else if (this.id == c.id)
+			return 0;
+		else
+			return -1;
 	}
 }
